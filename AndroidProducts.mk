e22aaac01a5c26f2eddab0bfb8d6bf7d919d2ca5
ViShal69x <tk5762996@gmail.com>
2024-05-24 16:31:56 +0200

    Initial LMOdroid bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fogos.mk
+    $(LOCAL_DIR)/lmodroid_fogos.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fogos-user \
-    lineage_fogos-userdebug \
-    lineage_fogos-eng
+    lmodroid_fogos-user \
+    lmodroid_fogos-userdebug \
+    lmodroid_fogos-eng
